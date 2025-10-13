0b257acf2dee01fde5b97152734f2cbc82cb7808
hubsxzry <zrydone1@gmail.com>
2025-10-13 07:31:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Toggle dark/light mode
+const toggle = document.getElementById("modeToggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  toggle.textContent = document.body.classList.contains("light") ? "☀️" : "🌙";
+});
